eeb5e4be0f089ed30a439165e0c5ed6c59037073
Omar <niazi.omark@gmail.com>
2022-10-30 18:42:44 -0400

    Finish directory structure and allow express to serve Vue app

---
+++ app.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+import path, { dirname } from 'path';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname,  '/frontend/dist/assets')));
+app.get('*', (req, res) => {
+    res.sendFile( path.join(__dirname, '/frontend/dist/index.html'));
+}); 
+
+app.listen(process.env.PORT || 3000);
