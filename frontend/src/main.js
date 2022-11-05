f5fca361f5fac04ca783bab338fc6a6168ac64c2
Omar <niazi.omark@gmail.com>
2022-11-04 23:07:31 -0400

    Begin working on sign-up and sign-in with persistent store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,39 @@
 import { createApp } from 'vue';
+import {createStore} from 'vuex';
 import App from './App.vue';
 import router from './router';
 
 import './assets/main.css';
 
+const store = createStore({
+    state(){
+        return {
+            username: window.localStorage.getItem('username') || '',
+            token: window.localStorage.getItem('token') || '',
+        };
+    },
+    mutations:{
+        logout(state){
+            state.username = '';
+            state.token = '';
+            window.localStorage.removeItem('token');
+            window.localStorage.removeItem('username');
+        }
+    },
+    actions:{
+        logout(state){
+            state.commit('logout');
+        },
+        login(state, user){
+
+        }
+    }
+
+});
+
 const app = createApp(App);
 
+app.use(store);
 app.use(router);
 
 app.mount('#app');
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,5 +1,35 @@
 <template>
+    <div>
+        <h1> Register </h1>
+        <form>
+            <div>
+                <label for="email"> Email Address: </label>
+                <input type="email" name="email" required/>
+            </div>
 
+            <div>
+                <label for="username"> Username: </label>
+                <input type="text" name="username"/>
+            </div>
+
+            <div>
+                <label for="password"> Password: </label>
+                <input type="text" name="password"/>
+                <input type="checkbox" @click.stop="showPassword()">Show Password
+            </div>
+
+            <div>
+                <label>
+
+                </label>
+            </div>
+
+            <div>
+                <input type="submit" value = "Register"/>
+            </div>
+
+        </form>
+    </div>
 </template>
 
 <script>
