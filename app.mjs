6891e87379d4559e55a918ce3b536eff8848df8c
Omar <niazi.omark@gmail.com>
2022-11-04 23:34:01 -0400

    Add router to app.mjs

--- app.mjs
+++ app.mjs
@@ -3,11 +3,15 @@ import path, { dirname } from 'path';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 
+import UserRouter from './backend/routers/users.mjs';
+
 dotenv.config();
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+app.use('/api/users', UserRouter);
 
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 app.use(express.static(path.join(__dirname,  '/frontend/dist/assets')));
